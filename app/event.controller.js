bccacd42c8ee0da294c5b5c79ac0a6f97943498a
TheGabCode <jdgonzales7@up.edu.ph>
2017-03-26 12:08:43 +0800

    added app folder for app.module.js and frontend controller.js

---
+++ app/event.controller.js
@@ -0,0 +1,80 @@
+(function(){
+'use strict'
+angular
+.module('mainApp')
+.controller('eventController', eventController);
+
+function eventController($http){
+    var vm = this;
+    
+    vm.userId;
+    vm.eventName = "";
+    vm.dateStart;
+    vm.dateEnd;
+    vm.addEvent = addEvent;
+    vm.allEvents = [];
+    vm.viewAllEvent = viewAllEvent;
+
+function addEvent() {
+    var newEvent = {
+        user_id : vm.userId,
+        event_name : vm.eventName,
+        date_start : vm.dateStart,
+        date_end : vm.dateEnd
+}
+$http
+    .post('/addEvent', newEvent)
+    .then(function(response){
+        console.log(response.data);
+        console.log('Success! Event Added!')
+},
+function(response){
+    console.log("Error :(");
+});
+}
+
+
+
+function viewAllEvent() {
+    
+$http
+    .get('/viewAllEvent')
+    .then(function(response){
+    	vm.allEvents = response.data;
+        console.log(response.data);
+        console.log('Viewing All Events!')
+},
+function(response){
+	console.log("error");	
+});
+}
+
+function viewEvent(id){
+
+    $http
+        .get('/viewEvent/' + id)
+        .then(function(response){
+            vm.allEvents = response.data;
+            console.log(response.data);
+            console.log('Viewing event ' + response.data.event_name);
+        })
+}
+
+
+function deleteEvent(id) {
+    
+$http
+    .delete('/deleteSport/'+id)
+    .then(function(response){
+        console.log('Event deleted')
+},
+function(response){
+	console.log("error");	
+});
+}
+
+
+
+}	
+}
+)();
