75fc1f35896c94bcb868b6ed5d8315c534790c52
Zoe Villanueva <zoektv@gmail.com>
2017-03-27 21:14:15 +0800

    Fixed Conventions

--- routes/router.js
+++ routes/router.js
@@ -14,11 +14,11 @@ const express = require('express');
 const router = express.Router();
 
 //module.exports = (router) => {
-router.post     ('/event',      eventController.addEvent); 
-router.get      ('/event/:event_id', eventController.viewEvent);
-router.get      ('/event',   eventController.viewAllEvent);
-router.put      ('/event',    eventController.updateEvent);
-router.delete   ('/event/:event_id',    eventController.deleteEvent);
+router.post     ('/events',      eventController.addEvent); 
+router.get      ('/events/:event_id', eventController.viewEvent);
+router.get      ('/events',   eventController.viewAllEvent);
+router.put      ('/events',    eventController.updateEvent);
+router.delete   ('/events/:event_id',    eventController.deleteEvent);
 router.post     ('/addTeam',       teamController.addTeam);
 router.get      ('/viewTeam/:team_id', teamController.viewTeam);
 router.get      ('/viewAllTeam',   teamController.viewAllTeam);
