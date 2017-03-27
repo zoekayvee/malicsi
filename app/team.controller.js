83a352507f7439bc42e85b204f09f6d6f7605df3
njisip <cyber2103@gmail.com>
2017-03-28 05:34:55 +0800

    added team.controller.js

---
+++ app/team.controller.js
@@ -0,0 +1,127 @@
+(function () {
+    'use strict'
+    angular
+        .module('mainApp')
+        .controller('teamController', teamController);
+
+    // router.post     ('/addTeam',       teamController.addTeam);
+    // router.get      ('/viewTeam/:team_id', teamController.viewTeam);
+    // router.get      ('/viewAllTeam',   teamController.viewAllTeam);
+    // router.put      ('/updateTeam',     teamController.updateTeam);
+    // router.delete   ('/deleteTeam/:team_id', teamController.deleteTeam);
+    // router.post     ('/teamJoinEvent',  teamController.teamJoinEvent);
+    // router.post     ('/teamPlayGame',   teamController.teamPlayGame);
+
+    function teamController($http) {
+        var vm = this;
+        
+        vm.teams = [];
+        vm.teamName = "";
+        vm.newTeamName = "";
+
+        function viewTeam(teamId) {
+            $http
+            .get('/viewTeam/' + teamId)
+            .then(function (response) {
+                vm.teams = response.data;
+                console.log(response.data);
+                console.log('Viewing team ' + response.data.team_name)
+            },
+            function (err) {
+                console.log(err);
+            });
+        }
+
+        function viewAllTeam() {
+            $http
+            .get('/viewAllTeam/')
+            .then(function (response) {
+                vm.teams = response.data;
+                console.log(response.data);
+                console.log('Viewing all teams')
+            },
+            function (err) {
+                console.log(err);
+            });
+        }
+
+        function addTeam() {
+            var newTeam = {
+                team_name = vm.teamName
+            }
+
+            $http
+            .post('/addTeam', newTeam)
+            .then(function (response) {
+                console.log(response.data);
+                console.log('Success! Team Added!')
+            },
+            function (err) {
+                console.log(err);
+            });
+        }
+
+        function updateTeam(teamId) {
+            var newTeam = {
+                team_id = team_id,
+                team_name = vm.newTeamName
+            }
+
+            $http
+            .put('/updateTeam', newTeam)
+            .then(function (response) {
+                console.log(response.data);
+                console.log('Success! Team Updated!')
+            },
+            function (err) {
+                console.log(err);
+            });
+        }
+
+        function deleteTeam(teamId) {
+            $http
+            .delete('/deleteTeam/' + teamId)
+            .then(function (response) {
+                console.log('Team deleted')
+            },
+            function (err) {
+                console.log(err);
+            });
+        }
+
+        function teamJoinEvent(teamId, eventId) {
+            var joinEvent = {
+                team_id: teamId,
+                event_id: eventId
+            }
+
+            $http
+            .post('/teamJoinEvent', joinEvent)
+            .then(function (response) {
+                console.log(response.data);
+                console.log('Success! Team Join Event!')
+            },
+            function (err) {
+                console.log(err);
+            });
+        }
+
+        function teamPlayGame(teamId, gameId) {
+            var playGame = {
+                team_id: teamId,
+                game_id: gameId
+            }
+
+            $http
+            .post('/teamPlayGame', playGame)
+            .then(function (response) {
+                console.log(response.data);
+                console.log('Success! Team Play Game!')
+            },
+            function (err) {
+                console.log(err);
+            });
+        }
+    }
+}
+)();
