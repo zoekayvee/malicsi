b3d777e33c0f46d6cb6bcd2768445b4df95e1a51
gelloguiam <acguiam@gmail.com>
2017-03-28 13:22:07 +0800

    added angular router

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 'use strict'
 
-var path = require('path');
 var express = require('express');
 var router = require('./routes/router');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
 
 var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'index'}));
-app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,8 +16,8 @@ app.use(session({
 	saveUninitialized: false
 }));
 
+app.use(express.static(__dirname+'/bower_components'));
 app.use(express.static(__dirname+'/public'));
-// app.use(express.static('public'));
 app.use(express.static(__dirname+'/app'));
 app.use(router);
 
