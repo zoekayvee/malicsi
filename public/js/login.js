82163c8c29999cbc588420ed22afe15e09fb7246
gelloguiam <acguiam@gmail.com>
2017-04-07 20:43:30 +0800

    added login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,43 @@
-
-  $(document)
-    .ready(function() {
-      $('.ui.form')
-        .form({
-          fields: {
+$(document).ready(function() {
+    $(".ui.form").form({
+        fields: {
             username: {
-              identifier  : 'username',
-              rules: [
-                {
-                  type   : 'empty',
-                  prompt : 'Please enter your username'
-                }
-              ]
+                identifier  : "username",
+                rules: [
+                    {
+                        type   : "empty",
+                        prompt : "Please enter your username"
+                    }
+                ]
             },
             password: {
-              identifier  : 'password',
-              rules: [
-                {
-                  type   : 'empty',
-                  prompt : 'Please enter your password'
-                }
-              ]
+                identifier  : "password",
+                rules: [
+                    {
+                        type   : "empty",
+                        prompt : "Please enter your password"
+                    }
+                ]
             },
-        },   
-		  inline : true,
-	   	on     : 'blur'
-        })
-      ;
-    })
-  ;
+        },
+        inline : true,
+        on     : "blur"
+    });
 
-$('select.dropdown')
-  .dropdown()
-;
+
+    $("select.dropdown").dropdown();
+    $("#page-navigation").hide();
+    $("#page-footer").hide();
+
+    $("#register-trigger").click(function(){
+        $("#login-div").slideUp("fast");
+        $("#register-div").slideDown("fast");
+    });
+
+    $("#login-trigger").click(function(){
+        $("#login-div").slideDown("fast");
+        $("#register-div").slideUp("fast");
+    });
+
+
+});
