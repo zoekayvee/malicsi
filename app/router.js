0b01803f566c42c07fc4ec41c7db2b2cdbf6cce9
gelloguiam <acguiam@gmail.com>
2017-04-16 20:19:05 +0800

    cleared router convention

--- app/router.js
+++ app/router.js
@@ -10,19 +10,9 @@
 					templateUrl: 'layouts/login.html',
 					controller:'userController'
 			})
-			.when('/teams/:team_id',{
-				templateUrl: 'layouts/user-view-team.html'
-			})
-			.when('/user/teams', {
-				templateUrl: 'layouts/user-view-teams.html'
-			})
-			.when('/users', {
-				templateUrl: 'layouts/admin-all-users.html'
-			})
-			.when('/register', {
-				templateUrl: 'layouts/register.html'
-			})
-            .when('/user/home', {
+
+			//User Account Routers
+			.when('/user/home', {
 				templateUrl: 'layouts/user-dashboard.html'
 			})
 			.when('/user/profile', {
@@ -31,99 +21,72 @@
 			.when('/user/activity-log', {
 				templateUrl: 'layouts/user-activity-log.html'
 			})
-			.when('/user/new-event', {
-				templateUrl: 'layouts/user-create-event.html'
-			})
-			.when('/user/event', {
-				templateUrl: 'layouts/user-event.html',
-				controller: 'eventController',
-				controllerAs: 'event'
-			})
-			.when('/user/events', {
+
+			//Events Routers
+			.when('/event', {
 				templateUrl: 'layouts/user-view-all-events.html',
 				controller: 'eventController',
 				controllerAs: 'event'
 			})
-			.when('/event',{
-				templateUrl:'layouts/user-event.html'
-			})
-			.when('/events/:event_id',{
-				templateUrl:'layouts/user-event.html',
+			.when('/event/:event_id',{
+				templateUrl:'layouts/user-view-event.html',
 				controller: 'eventController',
 				controllerAs: 'event'
 			})
-
-			
-
-			.when('/user/event/:event_id/scoreboard', {
+			.when('/event/:event_id/scoreboard', {
 				templateUrl: 'layouts/user-scoreboard-page.html',
 				controller:'userGameSchedController',
 				controllerAs:'UGSC'
 			})
-			.when('/user/game/:game_id', {
-				templateUrl: 'layouts/user-game-page.html',
-				controller:'gameController',
-				controllerAs:'game'
-			})
-			.when('/user/gameSched', {
+			.when('/event/game-schedule', {
 				templateUrl: 'layouts/user-game-sched.html',
 				controller:'userGameSchedController',
 				controllerAs:'UGSC'
 			})
 
-
-
-			.when('/user/game', {
+			//Game Routers
+			.when('/game/:game_id', {
 				templateUrl: 'layouts/user-game-page.html',
 				controller:'gameController',
 				controllerAs:'game'
 			})
-			.when('/user/event/:event_id'	, {
-				templateUrl: 'layouts/user-event-sports.html',
-				controller: 'userEventController',
-				controllerAs: 'event'
+
+			//Teams Routers
+			.when('team', {
+				templateUrl: 'layouts/user-view-all-teams.html'
 			})
-			.when('/user/event/:event_id/scoreboard', {
-				templateUrl: 'layouts/user-scoreboard-page.html',
-				controller:'userGameSchedController',
-				controllerAs:'UGSC'
+			.when('/team/:team_id',{
+				templateUrl: 'layouts/user-view-team.html'
 			})
-			.when('/user/game/:game_id', {
-				templateUrl: 'layouts/user-game-page.html',
-				controller:'gameController',
-				controllerAs:'game'
-			})
-			.when('/user/gameSched', {
-				templateUrl: 'layouts/user-game-sched.html',
-				controller:'userGameSchedController',
-				controllerAs:'UGSC'
-			})
-			.when('/user/team', {
-				templateUrl: 'layouts/user-view-teams.html'
-			})
-			.when('/user/search', {
+
+			//Search Routers
+			.when('/search', {
 				templateUrl: 'layouts/user-search-page.html'
 			})
-			.when('/admin/all-users', {
-				templateUrl: 'layouts/admin-all-users.html'
-			})
-			.when('/admin/all-events', {
-				templateUrl: 'layouts/admin-all-events.html'
-			})
-			.when('/admin/all-sponsors', {
-				templateUrl: 'layouts/admin-all-sponsors.html'
-			})
-			.when('/admin/all-games', {
-				templateUrl: 'layouts/admin-all-games.html'
-			})
-			.when('/admin/all-sports', {
-				templateUrl: 'layouts/admin-all-sports.html'
-			})
+
+			//Admin Routers
 			.when('/admin', {
 				templateUrl: 'layouts/admin-homepage.html'
 			})
+			.when('/admin/users', {
+				templateUrl: 'layouts/admin-all-users.html'
+			})
+			.when('/admin/events', {
+				templateUrl: 'layouts/admin-all-events.html'
+			})
+			.when('/admin/sponsors', {
+				templateUrl: 'layouts/admin-all-sponsors.html'
+			})
+			.when('/admin/games', {
+				templateUrl: 'layouts/admin-all-games.html'
+			})
+			.when('/admin/sports', {
+				templateUrl: 'layouts/admin-all-sports.html'
+			})
+
+			//Catch Case
 			.otherwise({
-				templateUrl: 'layouts/error.html'
+				templateUrl: 'layouts/error-404.html'
 			})
 	}
 })();
