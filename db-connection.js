8a544f10f8d9aed61517dae09409a43dd8d74bbc
Zoe Villanueva <zoektv@gmail.com>
2017-04-23 18:09:23 +0800

    Universalised db-connection

--- db-connection.js
+++ db-connection.js
@@ -2,8 +2,8 @@ var Client = require('mariasql');
 
 var c = new Client({
   host: '127.0.0.1',
-  user: 'root',
-  password: 'bossing',
+  user: 'projectOneTwoEight',
+  password: 'password',
   db: 'malicsiDB'
 });
 c.on('ready', function(){
