bc9fdec5a38ab0140ae532e5ab35cfef69578e68
jrbvaldez <jrbvaldez@gmail.com>
2017-04-26 20:28:20 +0800

    Fixed guest landing

--- app/guest.controller.js
+++ app/guest.controller.js
@@ -6,8 +6,34 @@
 
 	function guestController($http){
 		var vm = this;
+		vm.clicked = clicked;
 
 		vm.allGames = [];
+		vm.hasUser=null;
+		vm.isAdmin=null;
+
+		$http   
+            .get('/user_loggedin') 
+            .then(function(response) {
+            	if (response.data){
+            		vm.hasUser=true;
+            	}
+            	else{
+            		vm.hasUser=false;
+            	}
+            });
+
+        $http   
+            .get('/user_type_loggedin') 
+            .then(function(response) {
+            	vm.user_type = response.data;
+                if (response.data == 'admin'){
+                   vm.isAdmin = true;
+                }
+                else{
+                	vm.isAdmin = false;
+                }
+            });
 
 		$http   
             .get('/viewGames') 
@@ -19,6 +45,10 @@
                 	console.log("ERROR!");
                 }
             })
+
+        function clicked(id){
+        	window.location.href = '#!/game/' + id;
+        }
 	}
 
 })();
