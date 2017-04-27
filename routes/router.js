5e2532ca5bc560a300b64a634ba6943e277232f3
Yves <ydstaana@up.edu.ph>
2017-04-28 01:16:15 +0800

    scores of games can be viewed

--- routes/router.js
+++ routes/router.js
@@ -142,7 +142,6 @@ router.post('/ranking/:sport_id', 			gameController.getRanking);
 router.get('/overallranking/:event_id', 	gameController.getOverallRanking);
 router.get('/events/:event_id/games', 	    gameController.viewGamesByEvent);
 router.get('/events/:event_id/current_games', 	    gameController.viewCurrentGamesByEvent);
-router.get('/events/:event_id/upcoming_games', 	    gameController.viewCurrentGamesByEvent);
 router.post('/bet/:user_id', 				gameController.bet);
 router.get('/bet/:user_id/:game_id', 		gameController.betStatus);
 router.get('/scores/:game_id/:team_id', 	gameController.getScores);
