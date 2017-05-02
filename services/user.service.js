871ef4cf159a8bd7688340601e11100f062a34d0
alseco <alseco@up.edu.ph>
2017-05-02 22:13:15 +0800

    added event status on list of events

--- services/user.service.js
+++ services/user.service.js
@@ -182,7 +182,7 @@ exports.viewUserInterests = (req,res) => {
 }
 
 exports.viewUserEvents = (req,res) => {
-	const query_string = "SELECT username,event_name,event_id,DATE_FORMAT(date_start,'%M %e %Y') Date FROM users NATURAL JOIN event WHERE user_id = ?";
+	const query_string = "SELECT username,event_name,event_id,DATE_FORMAT(date_start,'%M %e %Y') Date,status FROM users NATURAL JOIN event WHERE user_id = ?";
 	const req_data = [req.params.user_id]
 
 	connection.query(query_string, req_data, (err,result)=>{
